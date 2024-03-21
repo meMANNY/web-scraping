b7964efc8d23fcf8b450bf281317d4223be83296
Aman Tripathi <amanttripathi02@gmail.com>
2024-03-22 01:42:13 +0530

    "this is the next commit"

--- web-scraper/components/SearchBar.tsx
+++ web-scraper/components/SearchBar.tsx
@@ -36,21 +36,23 @@ const SearchBar = () => {
         
     }
 
-  return (
+
+
+return (
     <div>
-    <form onSubmit={handleSubmit} className=" flex flex-wrap gap-4 mt-12">
-        <input 
-        type="text"
-        value={searchPrompt}
-        onChange={(e) => setSearchPrompt(e.target.value)}
-        placeholder="Enter product link"
-        className="searchbar-input"
-        />
-        <button className="searchbar-btn" type="submit" disabled = {searchPrompt === ''}>
-            {isLoading ? "Searching" : 'Search'}</button>
-    </form>        
+        <form onSubmit={handleSubmit} className=" flex flex-wrap gap-4 mt-12">
+                <input 
+                type="text"
+                value={searchPrompt}
+                onChange={(e) => setSearchPrompt(e.target.value)}
+                placeholder="Enter product link"
+                className="searchbar-input"
+                />
+                <button className="searchbar-btn" type="submit" disabled = {searchPrompt === ''}>
+                        {isLoading ? "Searching" : 'Search'}</button>
+        </form>        
     </div>
-  )
+)
 }
 
 export default SearchBar
