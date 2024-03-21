b474a54fa605a5b64f4a451cb74c5b139af36c6a
Aman Tripathi <amanttripathi02@gmail.com>
2024-03-22 01:52:50 +0530

    "complete scrape logic commited with types"

--- web-scraper/lib/scraper/index.ts
+++ web-scraper/lib/scraper/index.ts
@@ -1,52 +1,85 @@
-import axios from "axios";
-import * as cheerio from "cheerio";
-import { extractCurrency, extractprice } from "../utils";
-export async function scrapeAmazonProduct(productUrl: string){
-    if(!productUrl) throw new Error('Please enter a valid product URL');
+"use server"
 
-    // curl --proxy brd.superproxy.io:22225 --proxy-user brd-customer-hl_ee99a3ee-zone-pricewise:quo49d3cunhb -k https://lumtest.com/myip.json
-    try {
-        // scrape the product
-        //Brightdata proxy configuration
-        const username = String(process.env.USERNAME);
-        const password = String(process.env.PASSWORD);
-        const port = 22225;
-        const session_id = (100000*Math.random())|0;
-        
-        const options = {
-            auth: {
-                username: `${username}-session-${session_id}`,
-                password,},
-                host: 'brd.superproxy.io',
-                port,
-                rejectUnauthorized: false,};
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+import { extractCurrency, extractDescription, extractPrice } from '../utils';
 
-                try {
-                    //fetch the product page
-                    const response = await axios.get(productUrl, options);
-                    const $ = cheerio.load(response.data);
+export async function scrapeAmazonProduct(url: string) {
+  if(!url) return;
 
-                    //extract the product details
-                    const title = $('#productTitle').text().trim();
-                    const price = extractprice(
-                        $('.priceToPay span.a-price-whole'),
-                        $('a.size.base.a-color-price'),
-                        $('.a-button-selected .a-color-base')
-                    );
-                    
-                    const outOfStock = $('#availability').text().includes('Currently unavailable');
-                    const image = $('#imgBlkFront').attr('data-a-dynamic-image') || $('#landingImage').attr('data-a-dynamic-image') 
-                    || '';
+  // BrightData proxy configuration
+  const username = String(process.env.BRIGHT_DATA_USERNAME);
+  const password = String(process.env.BRIGHT_DATA_PASSWORD);
+  const port = 22225;
+  const session_id = (1000000 * Math.random()) | 0;
 
-                    const imageUrls = Object.keys(JSON.parse(image));
+  const options = {
+    auth: {
+      username: `${username}-session-${session_id}`,
+      password,
+    },
+    host: 'brd.superproxy.io',
+    port,
+    rejectUnauthorized: false,
+  }
 
-                    const currency = extractCurrency($('.a-price-symbol'));
-                } catch (error: any) {
-                    throw new error('An error occured while trying to scrape the product');
-                }
-        
-                        
-    } catch (error: any) {
-        throw new Error('An error occured while trying to scrape the product');
+  try {
+    // Fetch the product page
+    const response = await axios.get(url, options);
+    const $ = cheerio.load(response.data);
+
+    // Extract the product title
+    const title = $('#productTitle').text().trim();
+    const currentPrice = extractPrice(
+      $('.priceToPay span.a-price-whole'),
+      $('.a.size.base.a-color-price'),
+      $('.a-button-selected .a-color-base'),
+    );
+
+    const originalPrice = extractPrice(
+      $('#priceblock_ourprice'),
+      $('.a-price.a-text-price span.a-offscreen'),
+      $('#listPrice'),
+      $('#priceblock_dealprice'),
+      $('.a-size-base.a-color-price')
+    );
+
+    const outOfStock = $('#availability span').text().trim().toLowerCase() === 'currently unavailable';
+
+    const images = 
+      $('#imgBlkFront').attr('data-a-dynamic-image') || 
+      $('#landingImage').attr('data-a-dynamic-image') ||
+      '{}'
+
+    const imageUrls = Object.keys(JSON.parse(images));
+
+    const currency = extractCurrency($('.a-price-symbol'))
+    const discountRate = $('.savingsPercentage').text().replace(/[-%]/g, "");
+
+    const description = extractDescription($)
+
+    // Construct data object with scraped information
+    const data = {
+      url,
+      currency: currency || '$',
+      image: imageUrls[0],
+      title,
+      currentPrice: Number(currentPrice) || Number(originalPrice),
+      originalPrice: Number(originalPrice) || Number(currentPrice),
+      priceHistory: [],
+      discountRate: Number(discountRate),
+      category: 'category',
+      reviewsCount:100,
+      stars: 4.5,
+      isOutOfStock: outOfStock,
+      description,
+      lowestPrice: Number(currentPrice) || Number(originalPrice),
+      highestPrice: Number(originalPrice) || Number(currentPrice),
+      averagePrice: Number(currentPrice) || Number(originalPrice),
     }
+
+    return data;
+  } catch (error: any) {
+    console.log(error);
+  }
 }
