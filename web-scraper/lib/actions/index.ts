e11623dd128876a2165cee1038b5ad4feaf1d66b
Aman Tripathi <amanttripathi02@gmail.com>
2024-03-22 02:03:35 +0530

    "installed mongoose to connect to db"

--- web-scraper/lib/actions/index.ts
+++ web-scraper/lib/actions/index.ts
@@ -1,13 +1,18 @@
 "use server"
 
+import { connectToDatabase } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
 
 export async function scrapeAndStoreProduct(productUrl: string){
     if(!productUrl) throw new Error('Please enter a valid product URL');
 
     try {
+        connectToDatabase();
         const scrapedProduct = await scrapeAmazonProduct(productUrl);
+        if(!scrapedProduct) throw new Error('An error occured while trying to scrape the product');
         
+        // Store the product in the database
+
     } catch (error: any) {
         throw new Error('An error occured while trying to scrape the product');
     }
---
+++ web-scraper/lib/mongoose.ts
@@ -0,0 +1,7 @@
+import moongoose from 'mongoose';
+
+let iscConnected = false;// variable to track the connection status;
+
+export const connectToDatabase = async () => {
+    
+}
