28e01bed3044979aa32a99e7a1ccc375f617ed00
Aman Tripathi <amanttripathi02@gmail.com>
2024-04-03 22:48:32 +0530

    "this is the mongo connection file"

--- web-scraper/lib/actions/index.ts
+++ web-scraper/lib/actions/index.ts
@@ -1,17 +1,42 @@
 "use server"
 
+import { revalidatePath } from "next/cache";
+import Product from "../models/product.model";
 import { connectToDatabase } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
+import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
 
 export async function scrapeAndStoreProduct(productUrl: string){
     if(!productUrl) throw new Error('Please enter a valid product URL');
 
     try {
         connectToDatabase();
-        const scrapedProduct = await scrapeAmazonProduct(productUrl);
+        const scrapedProduct: any = await scrapeAmazonProduct(productUrl);
         if(!scrapedProduct) throw new Error('An error occured while trying to scrape the product');
         
         // Store the product in the database
+
+        let product = scrapedProduct;
+        const existingProduct = await Product.findOne({url: scrapedProduct.url});
+
+        if(existingProduct){
+            const updatedPriceHistory: any = [...existingProduct.priceHistory, {price: scrapedProduct.currentPrice}];
+        product = {
+            ...scrapedProduct,
+            priceHistory: updatedPriceHistory,
+            lowestPrice: getLowestPrice(updatedPriceHistory),
+            highestPrice: getHighestPrice(updatedPriceHistory),
+            averagePrice: getAveragePrice(updatedPriceHistory)
+        }
+        }
+
+        const newProduct = await Product.findOneAndUpdate({
+            url: scrapedProduct.url},
+            product,
+            {new: true, upsert: true}
+        )
+
+        revalidatePath(`/product/${newProduct._id} `);
 
     } catch (error: any) {
         throw new Error('An error occured while trying to scrape the product');
--- web-scraper/lib/mongoose.ts
+++ web-scraper/lib/mongoose.ts
@@ -1,7 +1,20 @@
-import moongoose from 'mongoose';
+import mongoose from 'mongoose';
 
-let iscConnected = false;// variable to track the connection status;
+let isConnected = false;// variable to track the connection status;
 
 export const connectToDatabase = async () => {
-    
+    mongoose.set('strictQuery', true);
+    if(!process.env.MONGODB_URI) throw new Error('Please provide a valid MongoDB URI');
+
+    if(isConnected) return;
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        isConnected = true;
+
+        }
+    catch (error:any) {
+        throw new Error('An error occured while trying to connect to the database');
+        
+    }
+
 }
